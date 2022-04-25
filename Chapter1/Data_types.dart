c90e6887ffde4c905731feecf92f7de0dfec2435
furkan <furkan.sonmez.eem@gmail.com>
2022-04-25 13:58:52 +0300

    main
    Signed-off-by: furkan <furkan.sonmez.eem@gmail.com>

--- Chapter1/Data_types.dart
+++ Chapter1/Data_types.dart
@@ -4,7 +4,7 @@ void main() {
 
     string ==> text 
     num  ==> all number. Decimal or normal isn't important 
-    int   ==> for normal number 
+    int   ==> for normal number  and you can use hexadecimal num 
     double ==> for decimal number 
     bool ==> for true false 
     var ==> you can use for all type value 
@@ -20,8 +20,19 @@ void main() {
   var phoneNum = 05533179778;
   var phoneModel = "iphone";
 
+  print(name + "Hello ");
+  print("$name $age years old"); // interpolation
+
   int weight = 76.5
       .toInt(); // if you want to change value type , you can use to.type() funcion
-  print(weight.runtimeType);
-  print(name + "Hello ");
+  ;
+  print("Weight value type : " +
+      weight.runtimeType
+          .toString()); // we have to use tostring() here because first part the print is text and we try to add number, you cant use string and num print
+  print(weight
+      .runtimeType); // but if you change in print or somewhere , this value type just change there.
+
+  print(
+      "Phone number lenght = ${phoneNum.toString().length}"); // if you want to use more than one methot you have to use {} 
+
 }

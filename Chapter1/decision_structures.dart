0a5a72ff68e74cab83a441744b3d013b155f6e33
furkan <furkan.sonmez.eem@gmail.com>
2022-04-25 15:01:05 +0300

    main
    Signed-off-by: furkan <furkan.sonmez.eem@gmail.com>

--- Chapter1/decision_structures.dart
+++ Chapter1/decision_structures.dart
@@ -1,3 +1,25 @@
 void main() {
-  print();
+  int num1 = 12;
+  int num2 = 24;
+
+  // normal if :
+  if (num1 > num2) {
+    print("$num1 biger than $num2 ");
+  } else if (num2 > num1) {
+    print("$num2 biger than $num1");
+  } else {
+    print("$num1 equals $num2  ");
+  }
+
+/* ternary if : 
+  use of : Decision ? expression1 : expression2 
+
+  you can use tenary if for null value detection 
+  expression1 ??  expression2           here if expression1 is null application will use expression2 
+  */
+  int num3 = 10;
+  int num4 = 11;
+  int smallNum;
+
+  if (num3 < num4) {}
 }

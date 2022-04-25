4a35b5ad8684ff2e9ec7cdca3fe35728bc2c9efc
furkan <furkan.sonmez.eem@gmail.com>
2022-04-25 16:29:28 +0300

    main
    Signed-off-by: furkan <furkan.sonmez.eem@gmail.com>

--- Chapter1/decision_structures.dart
+++ Chapter1/decision_structures.dart
@@ -11,15 +11,34 @@ void main() {
     print("$num1 equals $num2  ");
   }
 
-/* ternary if : 
-  use of : Decision ? expression1 : expression2 
-
-  you can use tenary if for null value detection 
-  expression1 ??  expression2           here if expression1 is null application will use expression2 
-  */
   int num3 = 10;
   int num4 = 11;
   int smallNum;
 
-  if (num3 < num4) {}
+  if (num3 < num4) {
+    smallNum = num3;
+  } else {
+    smallNum = num4;
+  }
+  print("small number is $smallNum");
+/* ternary if : 
+  use of : Decision ? expression1 : expression2 
+  */
+
+  int num5 = 15;
+  int num6 = 11;
+  int bigNum;
+
+  num6 > num5 ? bigNum = num6 : bigNum = num5;
+  print("big number is $bigNum");
+/* 
+you can use tenary if for null value detection 
+expression1 ??  expression2           here if expression1 is null application will use expression2 
+*/
+  String? name = null;
+  String? surname = 'sonmez';
+  String mesaj;
+
+  mesaj = name ?? surname;
+  print("Hello $mesaj");
 }

078b8bc1768e26af0d645631bb2b855669c44d7b
furkan <furkan.sonmez.eem@gmail.com>
2022-04-25 13:24:26 +0300

    main
    Signed-off-by: furkan <furkan.sonmez.eem@gmail.com>

--- Chapter1/Data_types.dart
+++ Chapter1/Data_types.dart
@@ -1,3 +1,26 @@
 void main() {
-   
+  /*
+    Basice Data Type : 
+
+    string ==> text 
+    num  ==> all number. Decimal or normal isn't important 
+    int   ==> for normal number 
+    double ==> for decimal number 
+    bool ==> for true false 
+    var ==> you can use for all type value 
+    
+    if you dont use value , the value's under will be blue line.
+
+  */
+  String name = "furkan";
+  num schoolNum = 180603024;
+  int age = 23;
+  double height = 1.75;
+  bool maritalStatus = false;
+  var phoneNum = 05533179778;
+  var phoneModel = "iphone";
+
+  int weight = 76.5.toInt(); // if you want to change value type , you can use to.type() funcion
+
+  print(name + "Hello ");
 }
